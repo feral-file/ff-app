a9d30c5696a056cca986382b36e0194455a98bf1
Anh Nguyen <anhnguyen@bitmark.com>
2026-01-28 11:39:47 +0700

    add app typography files and update gitignore

---
+++ lib/design/build/primitives.dart
@@ -0,0 +1,72 @@
+// Auto-generated file - documentation not required
+// ignore_for_file: public_member_api_docs
+//
+// primitives.dart
+//
+
+// Do not edit directly, this file was auto-generated.
+
+import 'dart:ui';
+
+class PrimitivesTokens {
+  PrimitivesTokens._();
+  
+  // colors
+  static const colorsBlack = Color(0xFF000000);
+  static const colorsDarkGrey = Color(0xFF2E2E2E);
+  static const colorsError = Color(0xFFD10000);
+  static const colorsGrey = Color(0xFFA0A0A0);
+  static const colorsHighlight = Color(0xFFECFF0C);
+  static const colorsInactive = Color(0xFF999999);
+  static const colorsLightBlue = Color(0xFFB9E5FF);
+  static const colorsLightGrey = Color(0xFFE3E3E3);
+  static const colorsLightRed = Color(0xFFF44336);
+  static const colorsWhite = Color(0xFFFFFFFF);
+  
+  // font families
+  static const fontFamiliesIbmPlexMono = 'IBMPlexMono';
+  static const fontFamiliesPpMori = 'PP Mori';
+  
+  // font sizes
+  static const fontSizesBody = 16;
+  static const fontSizesBodySmall = 14;
+  static const fontSizesCaption = 12;
+  static const fontSizesDisplay = 40;
+  static const fontSizesH1 = 28;
+  static const fontSizesH2 = 22;
+  static const fontSizesH3 = 18;
+  static const fontSizesH4 = 16;
+  static const fontSizesVerySmall = 8;
+  
+  // font weights
+  static const fontWeightsBold = 'Bold';
+  static const fontWeightsRegular = 'Regular';
+  
+  // icon sizes
+  static const iconSizesDefault = 16;
+  static const iconSizesLarge = 24;
+  static const iconSizesMedium = 20;
+  static const iconSizesSmall = 12;
+  
+  // letter spacings
+  static const letterSpacings0 = 0;
+  static const paragraphIndents0 = 0;
+  static const paragraphSpacings0 = 8;
+  static const paragraphSpacings1 = 24;
+
+  // spacing
+  static const spacingSetupPageHorizontal = 44;
+  static const spacingMinTouchTarget = 44;
+  static const spacingPageHorizontalDefault = 16;
+  static const spacingSpace1 = 4;
+  static const spacingSpace10 = 40;
+  static const spacingSpace12 = 48;
+  static const spacingSpace16 = 64;
+  static const spacingSpace20 = 80;
+  static const spacingSpace2 = 8;
+  static const spacingSpace3 = 12;
+  static const spacingSpace4 = 16;
+  static const spacingSpace5 = 20;
+  static const spacingSpace6 = 24;
+  static const spacingSpace8 = 32;
+}
---
+++ lib/design/build/typography.dart
@@ -0,0 +1,110 @@
+// Auto-generated file - documentation not required
+// ignore_for_file: public_member_api_docs
+//
+// typography.dart
+//
+
+// Do not edit directly, this file was auto-generated.
+
+class TypographyTokens {
+  TypographyTokens._();
+  
+  // Display
+  static const displayFontFamily = 'PP Mori';
+  static const displayFontSize = 40;
+  static const displayFontWeight = 'Bold';
+  static const displayLetterSpacing = 0;
+  static const displayLineHeight = 48.0;
+  static const displayParagraphIndent = 0;
+  static const displayParagraphSpacing = 24;
+  
+  // H1
+  static const h1FontFamily = 'PP Mori';
+  static const h1FontSize = 28;
+  static const h1FontWeight = 'Bold';
+  static const h1LetterSpacing = 0;
+  static const h1LineHeight = 33.6;
+  static const h1ParagraphIndent = 0;
+  static const h1ParagraphSpacing = 24;
+  
+  // H2
+  static const h2FontFamily = 'PP Mori';
+  static const h2FontSize = 22;
+  static const h2FontWeight = 'Bold';
+  static const h2LetterSpacing = 0;
+  static const h2LineHeight = 27.5;
+  static const h2ParagraphIndent = 0;
+  static const h2ParagraphSpacing = 24;
+  
+  // H3
+  static const h3FontFamily = 'PP Mori';
+  static const h3FontSize = 18;
+  static const h3FontWeight = 'Bold';
+  static const h3LetterSpacing = 0;
+  static const h3LineHeight = 22.5;
+  static const h3ParagraphIndent = 0;
+  static const h3ParagraphSpacing = 24;
+  
+  // H4
+  static const h4FontFamily = 'PP Mori';
+  static const h4FontSize = 16;
+  static const h4FontWeight = 'Bold';
+  static const h4LetterSpacing = 0;
+  static const h4LineHeight = 20.0;
+  static const h4ParagraphIndent = 0;
+  static const h4ParagraphSpacing = 8;
+  
+  // Body
+  static const bodyFontFamily = 'PP Mori';
+  static const bodyFontSize = 16;
+  static const bodyFontWeight = 'Regular';
+  static const bodyLetterSpacing = 0;
+  static const bodyLineHeight = 22.4;
+  static const bodyParagraphIndent = 0;
+  static const bodyParagraphSpacing = 8;
+  
+  // BodySmall
+  static const bodySmallFontFamily = 'PP Mori';
+  static const bodySmallFontSize = 14;
+  static const bodySmallFontWeight = 'Regular';
+  static const bodySmallLetterSpacing = 0;
+  static const bodySmallLineHeight = 19.6;
+  static const bodySmallParagraphIndent = 0;
+  static const bodySmallParagraphSpacing = 8;
+  
+  // Caption
+  static const captionFontFamily = 'PP Mori';
+  static const captionFontSize = 12;
+  static const captionFontWeight = 'Regular';
+  static const captionLetterSpacing = 0;
+  static const captionLineHeight = 18.0;
+  static const captionParagraphIndent = 0;
+  static const captionParagraphSpacing = 8;
+  
+  // VerySmall
+  static const verySmallFontFamily = 'PP Mori';
+  static const verySmallFontSize = 8;
+  static const verySmallFontWeight = 'Regular';
+  static const verySmallLetterSpacing = 0;
+  static const verySmallLineHeight = 11.2;
+  static const verySmallParagraphIndent = 0;
+  static const verySmallParagraphSpacing = 8;
+  
+  // Mono
+  static const monoFontFamily = 'IBMPlexMono';
+  static const monoFontSize = 16;
+  static const monoFontWeight = 'Regular';
+  static const monoLetterSpacing = 0;
+  static const monoLineHeight = 22.4;
+  static const monoParagraphIndent = 0;
+  static const monoParagraphSpacing = 8;
+  
+  // MonoSmall
+  static const monoSmallFontFamily = 'IBMPlexMono';
+  static const monoSmallFontSize = 14;
+  static const monoSmallFontWeight = 'Regular';
+  static const monoSmallLetterSpacing = 0;
+  static const monoSmallLineHeight = 19.6;
+  static const monoSmallParagraphIndent = 0;
+  static const monoSmallParagraphSpacing = 8;
+}
