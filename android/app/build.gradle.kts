13b287ff6ef1c17abf5d5dfe18596e66b08f0aad
Anh Nguyen <anhnguyen@bitmark.com>
2026-02-04 18:56:32 +0700

    update the workflow

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,11 +39,25 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("release.keystore")
+            storePassword = System.getenv("KEYSTORE_PASSWORD") ?: ""
+            keyAlias = System.getenv("KEY_ALIAS") ?: ""
+            keyPassword = System.getenv("KEY_PASSWORD") ?: ""
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
+    flavorDimensions += "tier"
+    productFlavors {
+        create("production") {
+            dimension = "tier"
         }
     }
 }
