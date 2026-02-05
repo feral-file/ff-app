e74e987e6a5c8722a040d4db73bc2e1dece71a60
Anh Nguyen <anhnguyen@bitmark.com>
2026-02-05 09:47:44 +0700

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,10 +5,17 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+import java.util.Properties
+import java.io.File
+
 android {
     namespace = "com.feralfile.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+
+    sourceSets {
+        getByName("main").java.srcDirs("src/main/kotlin")
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -16,57 +23,82 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 
     packaging {
         resources {
-            pickFirst("lib/arm64-v8a/libc++_shared.so")
-            pickFirst("lib/armeabi-v7a/libc++_shared.so")
-            pickFirst("lib/x86/libc++_shared.so")
-            pickFirst("lib/x86_64/libc++_shared.so")
+            pickFirsts.add("lib/arm64-v8a/libc++_shared.so")
+            pickFirsts.add("lib/armeabi-v7a/libc++_shared.so")
+            pickFirsts.add("lib/x86/libc++_shared.so")
+            pickFirsts.add("lib/x86_64/libc++_shared.so")
+            excludes.add("META-INF/*")
+        }
+        jniLibs {
+            pickFirsts.add("lib/arm64-v8a/libc++_shared.so")
+            pickFirsts.add("lib/armeabi-v7a/libc++_shared.so")
+            pickFirsts.add("lib/x86/libc++_shared.so")
+            pickFirsts.add("lib/x86_64/libc++_shared.so")
         }
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.feralfile.app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 33
-        targetSdk = 33
+        minSdk = 29
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        resourceConfigurations.addAll(listOf("en", "en-US"))
     }
 
     signingConfigs {
         create("release") {
-            val keystoreFile = file("../release.keystore")
+            val keystoreFile = File("${project.rootDir}/../release.keystore")
             if (keystoreFile.exists()) {
-                val props = java.util.Properties()
-                val propsFile = file("../release.properties")
+                val props = Properties()
+                val propsFile = File("${project.rootDir}/../release.properties")
                 if (propsFile.exists()) {
-                    props.load(propsFile.inputStream())
+                    propsFile.inputStream().use { props.load(it) }
                 }
                 storeFile = keystoreFile
-                storePassword = props.getProperty("storePassword", "") ?: ""
-                keyAlias = props.getProperty("keyAlias", "") ?: ""
-                keyPassword = props.getProperty("keyPassword", "") ?: ""
+                storePassword = props.getProperty("key.store.password", "")
+                keyAlias = props.getProperty("key.alias", "")
+                keyPassword = props.getProperty("key.alias.password", "")
             }
         }
     }
 
+    buildFeatures {
+        compose = true
+        viewBinding = true
+    }
+
     buildTypes {
         release {
+            isMinifyEnabled = true
+            isShrinkResources = true
             signingConfig = signingConfigs.getByName("release")
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
         }
     }
 
-    flavorDimensions += "tier"
+    flavorDimensions.add("env")
     productFlavors {
-        create("production") {
-            dimension = "tier"
+        create("development") {
+            dimension = "env"
+            applicationIdSuffix = ".inhouse"
+            resValue("string", "app_name", "Feral File (Dev)")
         }
+        create("production") {
+            dimension = "env"
+            resValue("string", "app_name", "Feral File")
+        }
+    }
+
+    lint {
+        checkReleaseBuilds = false
     }
 }
 
